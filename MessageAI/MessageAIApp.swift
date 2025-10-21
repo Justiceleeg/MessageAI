0a2f2c4e7008e119bc9ea695dd1a2453fa36c023
Justiceleeg <justicepwhite@gmail.com>
2025-10-20 21:25:07 -0500

    Initial Commit

---
+++ MessageAI/MessageAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageAIApp.swift
+//  MessageAI
+//
+//  Created by Justice Perez White on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MessageAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
